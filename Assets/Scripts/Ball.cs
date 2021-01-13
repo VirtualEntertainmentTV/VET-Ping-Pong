0aac60cf2bcaf63e6a80ff9fd7c993d9da7fdf23
VET <73408920+VirtualEntertainmentTV@users.noreply.github.com>
2021-01-13 05:07:43 -0500

    Add Unity Project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody MyBody;
+
+    private Vector3 InitialImpulse;
+
+    private Vector3 InitialPosition;
+
+    [SerializeField]
+    private float ImpulseForce = 10f;
+
+    public Text Player1_ScoreText, Player2_ScoreText;
+    private int p1_Score, p2_Score;
+
+    void Awake()
+    {
+        InitialPosition = transform.position;
+
+        MyBody = GetComponent<Rigidbody>();
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Random.Range(0, 2) > 0)
+        {
+            ImpulseForce *= -1f;
+        }
+
+        InitialImpulse = new Vector3(ImpulseForce, 0f, ImpulseForce);
+
+        StartCoroutine(BallStartMoving());
+    }
+
+    IEnumerator BallStartMoving()
+    {
+        yield return new WaitForSeconds(2f);
+        MyBody.AddForce(InitialImpulse, ForceMode.Impulse);
+    }
+
+    void SetScore()
+    {
+        Player1_ScoreText.text = "Player1; " + p1_Score;
+        Player2_ScoreText.text = "Player2; " + p2_Score;
+    }
+
+    void ResetMovement()
+    {
+        MyBody.velocity = Vector3.zero;
+        transform.position = InitialPosition;
+
+        StartCoroutine(BallStartMoving());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player 1 Score")
+        {
+            p2_Score++;
+            SetScore();
+
+            ResetMovement();
+        }
+
+        if (other.tag == "Player 2 Score")
+        {
+            p1_Score++;
+            SetScore();
+
+            ResetMovement();
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Player2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    public float speed = 20f;
+    public float max_X = 14.7f, min_X = -14.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        CheckBounds();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+
+    void CheckBounds()
+    {
+        Vector3 temp = transform.position;
+
+        if (temp.x > max_X)
+            temp.x = max_X;
+
+        if (temp.x < min_X)
+            temp.x = min_X;
+
+        transform.position = temp;
+    }
+
+
+} // Player 2
